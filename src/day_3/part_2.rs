3ee9ad3135443cdf668e8e3d398105723d9f5389
Omar2535 <omar2535@alumni.ubc.ca>
2022-12-04 02:54:55 -0500

    finished day3 part 2

--- src/day_3/part_2.rs
+++ src/day_3/part_2.rs
@@ -3,41 +3,65 @@ use std::io::{prelude::*, BufReader};
 use std::collections::HashMap;
 
 #[allow(dead_code)]
-pub fn part_1() {
+pub fn part_2() {
     let file_result = File::open("./data/day_3.txt");
     let file = file_result.unwrap();
 
     let reader = BufReader::new(file);
-    let mut both: Vec<Vec<char>> = Vec::new();
+    let mut same: Vec<Vec<char>> = Vec::new();
 
-    // go through elf sacks
+    // go through stuff
+    let mut i: u32 = 1;
+    let mut str1: String = String::from("");
+    let mut str2: String = String::from("");
+    let mut str3: String = String::from("");
     for linebuf in reader.lines() {
         let line: String = linebuf.unwrap();
 
-        // split into two strings
-        let first_part: String = line.chars().take(line.len()/2).collect();
-        let second_part: String = line.chars().skip(line.len()/2).take(line.len()-line.len()/2).collect();
+        if i % 3 == 2 {
+            str1 = line;
+            i += 1;
+            continue;
+        } else if i % 3 == 1 {
+            str2 = line;
+            i += 1;
+            continue;
+        } else if i % 3 == 0 {
+            str3 = line;
+            i += 1;
+        }
 
-        
         // collect list of characters that are the same
         let mut first_part_chars: HashMap<char, u32> = HashMap::new();
-        let mut inner_vec: Vec<char> = Vec::new();
-        for c in first_part.chars() {
+        for c in str1.chars() {
             first_part_chars.insert(c, 1);
         }
 
-        for c in second_part.chars() {
+        let mut second_part_chars: HashMap<char, u32> = HashMap::new();
+        for c in str2.chars() {
             if first_part_chars.contains_key(&c) {
+                second_part_chars.insert(c, 1);
+            }
+        }
+
+        let mut inner_vec: Vec<char> = Vec::new();
+        for c in str3.chars() {
+            if second_part_chars.contains_key(&c) {
                 inner_vec.push(c);
                 break;
             }
         }
-        both.push(inner_vec);
+        same.push(inner_vec);
+
+        // clear the strings
+        str1 = String::from("");
+        str2 = String::from("");
+        str3 = String::from("");
     }
 
     // add it up
     let mut sum: u32 = 0;
-    for inner_vec in both.iter() {
+    for inner_vec in same.iter() {
         for c in inner_vec.iter() {
             print!("char: {}, value: {}", c, get_value(*c));
             sum += get_value(*c);
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ mod day_2;
 mod day_3;
 
 fn main() {
-    day_3::part_1::part_1();
+    day_3::part_2::part_2();
 }
