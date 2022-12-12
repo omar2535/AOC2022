10e05e2bcf46385f60b03cd4d22fd3a89947c8a3
Omar2535 <omar2535@alumni.ubc.ca>
2022-12-12 16:54:18 -0500

    working on it

--- src/day_8/part_1.rs
+++ src/day_8/part_1.rs
@@ -9,15 +9,18 @@ pub fn part_1() {
     let reader = BufReader::new(file);
 
     // our matrices
-    let original: Vec<Vec<u8>> = Vec::new();
-    let up: Vec<Vec<u8>> = Vec::new();
-    let down: Vec<Vec<u8>> = Vec::new();
-    let left: Vec<Vec<u8>> = Vec::new();
-    let right: Vec<Vec<u8>> = Vec::new();
-
+    let mut original: Vec<Vec<u32>> = Vec::new();
+    let mut up: Vec<Vec<u32>> = Vec::new();
+    let mut down: Vec<Vec<u32>> = Vec::new();
+    let mut left: Vec<Vec<u32>> = Vec::new();
+    let mut right: Vec<Vec<u32>> = Vec::new();
+    
+    // create the original matrix
     for (index, linebuf) in reader.lines().enumerate() {
+        let mut row: Vec<u32> = vec![];
         for digit_str in linebuf.unwrap().chars() {
-            println!("{}", digit_str);
+            row.push(digit_str.to_digit(10).unwrap());
         }
+        original.push(row);
     }
 }
